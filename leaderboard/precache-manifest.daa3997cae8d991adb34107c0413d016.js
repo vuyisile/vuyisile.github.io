f1598888dc270c5ebe9c51d5d5ba5306351c910d
vuyisile <vuyisilemark@gmail.com>
2019-02-01 12:32:25 +0200

    replaced the leaderboard.html with the leaderboard folder.

---
+++ leaderboard/precache-manifest.daa3997cae8d991adb34107c0413d016.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "aa46ab28bd2bf80b756f",
+    "url": "/leaderboard/static/js/runtime~main.aa46ab28.js"
+  },
+  {
+    "revision": "b166afbd6f1e899db9cc",
+    "url": "/leaderboard/static/js/main.b166afbd.chunk.js"
+  },
+  {
+    "revision": "cf59732c1482a2c52339",
+    "url": "/leaderboard/static/js/1.cf59732c.chunk.js"
+  },
+  {
+    "revision": "b166afbd6f1e899db9cc",
+    "url": "/leaderboard/static/css/main.1d22a0e5.chunk.css"
+  },
+  {
+    "revision": "45cc294a72985b931760231b255382b5",
+    "url": "/leaderboard/index.html"
+  }
+];
